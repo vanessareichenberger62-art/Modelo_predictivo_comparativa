5c4167c5b4753573b2d1db3eb4e337a4403a8e8f
Vanessa Reichenberger | Web Developer <vanessareichenberger62@gmail.com>
2026-08-15 11:26:04 -0400

    Delete analisis_practica.py

--- analisis_practica.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel(r"C:\FREELANCER\Cursos para freelancer\MACHINE LEARNING_TEOREMA\MÓDULO 4_ANÁLISIS DE DATOS CON PYTHON\DATOS DE PRÁCTICA\agent_details_CALL_CENTER.xls")
-
-print(df.head())
